8839aa91cd50be9a4e41203adebf49b8fd3a5662
Ana Bergese <violetagele22@gmail.com>
2022-12-17 12:39:32 -0300

    deleted unnecesary types of typescript

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -16,17 +16,3 @@ export type ITask = {
   status: TaskStatus;
   id: string;
 };
-
-export type IFeel = "Ok" | "Good" | "Happy" | "Sad" | "Terrible";
-export type IFeelings = {
-  Feel_id: number;
-  Feeling: IFeel;
-  Image_URL: string;
-};
-
-export type IReqOptions = {
-  method: string;
-  headers: string;
-  body: string;
-  redirect: string;
-};
