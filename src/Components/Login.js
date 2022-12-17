7fcdf83271bf5b676e04a86dee5b9931c2011934
Ana Bergese <violetagele22@gmail.com>
2022-12-17 18:40:33 -0300

    styled landing page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,86 +1,52 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Background } from "./Styles/Login.styled";
+import {
+  StyledLanding,
+  StyledDiv,
+  StyledLogin,
+  StyledFeatures,
+  StyledGoal,
+  StyledCallButton,
+} from "./Styles/Login.styled";
 
 export const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return (
-    <Background>
-      <div>
+    <StyledLanding>
+      <StyledDiv>
+        <img src={require("../Images/navbar.png")} alt="app logotype" />
+      </StyledDiv>
+      <StyledLogin>
+        <h1>
+          Organizing your day activity
+          <br />
+          with Todo App
+        </h1>
+        <StyledCallButton onClick={() => loginWithRedirect()}>
+          Get started
+        </StyledCallButton>
+        <img src={require("../Images/organizing.png")} alt="app logotype" />
+      </StyledLogin>
+      <StyledFeatures>
+        <h2>Don’t let your day doing nothing</h2>
+        <img src={require("../Images/features.png")} alt="app logotype" />
+      </StyledFeatures>
+      <StyledGoal>
+        <img src={require("../Images/goal.png")} alt="app logotype" />
         <div>
-          <img></img>
-          <p>Todo App</p>
+          <h3>
+            Achieve your target <br />
+            and won your life
+          </h3>
+          <StyledCallButton onClick={() => loginWithRedirect()}>
+            Get Started
+          </StyledCallButton>
         </div>
-        <div>
-          <button onClick={() => loginWithRedirect()}>Login</button>
-          <button onClick={() => loginWithRedirect()}>Signup</button>
-        </div>
-      </div>
-      <h1>Organizing your day activity with Todo App</h1>
-      <div>
-        <button onClick={() => loginWithRedirect()}>Get started</button>
-        <img></img>
-      </div>
-      <h2>Don’t let your day doing nothing</h2>
-      <div>
-        <div>
-          <img></img>
-          <p>Small task</p>
-        </div>
-        <div>
-          <img></img>
-          <p>Write it</p>
-        </div>
-        <div>
-          <img></img>
-          <p>Do it</p>
-        </div>
-        <div>
-          <img></img>
-          <p>Repeat</p>
-        </div>
-      </div>
-      <div>
-        <div>
-          <img></img>
-        </div>
-        <div>
-          <h3>Achieve your target and won your life</h3>
-          <button onClick={() => loginWithRedirect()}>Get Started</button>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>
-            <img></img>
-            <p>Lets change your habit join with million people</p>
-          </div>
-          <div>
-            <p>DK Tech Company</p>
-            <p>Uma Street, Lost City, Aincard </p>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <p>Features</p>
-            <br></br>
-            <p>Lorem</p>
-            <p>Ipsum</p>
-            <p>Sit</p>
-            <p>Dolor</p>
-          </div>
-          <div>
-            <p>Pricing</p>
-            <br></br>
-            <p>Lorem</p>
-            <p>Ipsum</p>
-            <p>Sit</p>
-            <p>Dolor</p>
-          </div>
-        </div>
-      </div>
-    </Background>
+      </StyledGoal>
+      <StyledDiv>
+        <img src={require("../Images/footer.png")} alt="app logotype" />
+      </StyledDiv>
+    </StyledLanding>
   );
 };
--- src/Components/Styles/Login.styled.js
+++ src/Components/Styles/Login.styled.js
@@ -1,7 +1,92 @@
 import styled from "styled-components";
 
-export const Background = styled.div`
-  background: linear-gradient(299.75deg, #b9d5ff 0%, #f6d1fc 98.93%);
+export const StyledCallButton = styled.button`
+  height: 38px;
+  width: 160px;
+  left: 748px;
+  top: 364px;
+  border-radius: 8px;
+  border-style: none;
+  background-color: #ff4f5a;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+`;
+
+export const StyledLanding = styled.div`
   width: 100vw;
   height: 100vh;
+  margin: 1% 4%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  img {
+    width: 72%;
+  }
+`;
+
+export const StyledLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 1.8rem;
+    width: 35rem;
+    margin-top: 72px !important;
+    margin-bottom: 12px !important;
+  }
+
+  img {
+    width: 54%;
+    margin-top: 48px;
+  }
+`;
+
+export const StyledFeatures = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    font-size: 1.8rem;
+    width: 35rem;
+    margin-top: 180px !important;
+    margin-bottom: 56px !important;
+  }
+  img {
+    width: 64%;
+  }
+`;
+
+export const StyledGoal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 180px;
+  margin-bottom: 200px;
+
+  img {
+    margin-left: 200px;
+    margin-rigth: 180px;
+    width: 30%;
+  }
+
+  div {
+    margin-left: 60px;
+  }
+
+  h3 {
+    font-size: 1.8rem;
+    width: 35rem;
+    margin-top: 48px !important;
+    margin-bottom: 28px !important;
+  }
 `;
