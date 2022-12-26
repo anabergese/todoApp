690c7bf971091ae7dc5c1a6f1124b0d5ff9e0b12
Ana Bergese <violetagele22@gmail.com>
2022-12-26 14:46:08 -0300

    deleted styledlink from buttons.styled

--- src/Components/Styles/Buttons.styled.ts
+++ src/Components/Styles/Buttons.styled.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { IButtonsProps } from "../../Types/index";
 
 export const StyledButton = styled.button<IButtonsProps>`
