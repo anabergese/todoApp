1e62a8e894aaba7cab72acfcc260a5828c7131e9
Ana Bergese <violetagele22@gmail.com>
2023-01-16 12:54:59 -0300

    deleted alltasks context in Task component

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,46 +1,60 @@
 /* eslint-disable jsx-a11y/no-autofocus */
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState, useContext, FunctionComponent } from "react";
+import { useState, useEffect, useContext, FunctionComponent } from "react";
 import { FocusScope } from "react-aria";
 import Modal from "./Modal";
-import AlltasksContext from "../Contexts/AlltasksContext";
 import { TaskStatus, ITask } from "../Types/index";
 import { StyledTask, TitleTask, ContentTask } from "./Styles/Task.styled";
 import { StyledModal } from "./Styles/Modal.styled";
 import ThemeContext from "../Contexts/ThemeContext";
 import { StyledButton } from "./Styles/Buttons.styled";
-import { permanentDeleteRequest, updateRequest } from "./API Requests/Requests";
+import {
+  permanentDeleteRequest,
+  updateRequest,
+  getTaskRequest,
+} from "./API Requests/Requests";
 
 const Task: FunctionComponent = () => {
   const [showModal, setShowModal] = useState(false);
-  const [allTasks, setAllTasks] = useContext(AlltasksContext);
   const location = useLocation();
-  const taskProps = location.state as ITask;
   const toggleModal = () => setShowModal(!showModal);
   const [themes] = useContext(ThemeContext);
   const navigate = useNavigate();
+  // añades un estado que diga "toca refrescar":
+  const [shouldUpdate, setShouldUpdate] = useState(false);
+  const [taskProps, setTaskProps] = useState(location.state);
 
-  const tasksHandler = (status: TaskStatus) => {
-    const taskIndex = allTasks.findIndex((item) => item.id === taskProps.id);
-    const copy = [...allTasks];
-    copy[taskIndex].status = status;
-    taskProps.status = status;
-    setAllTasks(copy);
+  useEffect(() => {
+    if (shouldUpdate) {
+      // en este ejemplo hace fetch de la DB, pero
+      // también podrías hacer que en vez de decirle
+      // al componente "toca refrescar" le digas
+      // _qué_ refrescar (por evitar consultar la DB)
+      getTaskRequest(taskProps)
+        .then((result) => {
+          setTaskProps(result);
+        })
+        .catch((error) => console.log("error", error));
 
+      setShouldUpdate(false);
+    }
+  }, [shouldUpdate]);
+
+  const tasksHandler = (status: TaskStatus, taskProps: ITask) => {
     updateRequest(status, taskProps)
-      .then((result) => result)
+      .then((result) => {
+        setShouldUpdate(true);
+        return result;
+      })
       .catch((error) => console.log("error", error));
   };
 
-  const deleteHandler = () => {
-    tasksHandler("Deleted");
+  const deleteHandler = (taskProps: ITask) => {
+    tasksHandler("Deleted", taskProps);
     toggleModal();
   };
 
-  const permanentDeleteHandler = () => {
-    const copy = allTasks.filter((item) => item.id !== taskProps.id);
-    setAllTasks(copy);
-
+  const permanentDeleteHandler = (taskProps: ITask) => {
     permanentDeleteRequest(taskProps)
       .then((result) => {
         result;
@@ -49,9 +63,10 @@ const Task: FunctionComponent = () => {
       .catch((error) => console.log("error", error));
   };
 
-  const completeHandler = () => {
-    if (taskProps.status == "Uncompleted") tasksHandler("Completed");
-    else if (taskProps.status == "Completed") tasksHandler("Uncompleted");
+  const completeHandler = (taskProps: ITask) => {
+    if (taskProps.status == "Uncompleted") tasksHandler("Completed", taskProps);
+    else if (taskProps.status == "Completed")
+      tasksHandler("Uncompleted", taskProps);
   };
 
   return (
@@ -65,7 +80,9 @@ const Task: FunctionComponent = () => {
             <StyledButton
               onClick={
                 taskProps.status === "Deleted"
-                  ? permanentDeleteHandler
+                  ? () => {
+                      permanentDeleteHandler(taskProps);
+                    }
                   : toggleModal
               }
               theme={themes}
@@ -75,7 +92,7 @@ const Task: FunctionComponent = () => {
             <StyledButton
               theme={themes}
               onClick={() => {
-                completeHandler();
+                completeHandler(taskProps);
               }}
             >
               {taskProps.status === "Completed" ||
@@ -94,7 +111,7 @@ const Task: FunctionComponent = () => {
                       <StyledButton
                         theme={themes}
                         onClick={() => {
-                          deleteHandler();
+                          deleteHandler(taskProps);
                         }}
                       >
                         Yes
@@ -123,7 +140,7 @@ const Task: FunctionComponent = () => {
                   : ""
               }
             >
-              <strong> Status: </strong>
+              <strong>Status:</strong>
               {taskProps.status}
             </p>
           </div>
